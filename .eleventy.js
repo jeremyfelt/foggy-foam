4ab687806afdb343f78312395864a700b6ed7a97
jeremyfelt <jeremy.felt@gmail.com>
2021-05-10 17:38:28 -0700

    Add initial Eleventy structure

---
+++ .eleventy.js
@@ -0,0 +1,40 @@
+module.exports = ( eleventyConfig ) => {
+	console.log( eleventyConfig );
+
+	let markdownIt = require("markdown-it");
+	let options = {
+	  html: true,
+	  breaks: true,
+	  linkify: false
+	};
+
+	const mdWikilinksOptions = {
+        baseURL: "/",
+        makeAllLinksAbsolute: true,
+        uriSuffix: "/",
+        linkPattern: /\[\[([\w\s/-]+)(\|([\w\s/]+))?\]\]/,
+        postProcessPageName: (pageName) => {
+            return pageName.trim().toLowerCase().replace(/\s/g, "-");
+        }
+    }
+
+	const md = markdownIt( options ).use( require( "@kwvanderlinde/markdown-it-wikilinks")(mdWikilinksOptions));
+
+	eleventyConfig.addFilter("markdownify", string => {
+        return md.render(string)
+    })
+
+    eleventyConfig.setLibrary('md', md);
+
+	eleventyConfig.addPassthroughCopy('assets');
+
+	return {
+		dir: {
+		  input: ".",
+		  output: "_site",
+		  includes: "_includes",
+		  layouts: "_layouts",
+		},
+		passthroughFileCopy: true
+	};
+}
